1e4a5594df7a8a6e00f8e1fcdbd57b01093daddc
Rick Housley <rickyhousley+github@gmail.com>
2014-11-30 16:55:39 -0500

    server working, features complete.

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import sys
+import time
 import random
+
 from scapy.all import * 	# Packet manipulation lib
 import logging
 import datetime
 import argparse 			# Command line menu
-from unauthored import *	# Import unauthored code snippets
+
 
 def main(args):	
 	# Inititate Logging
 	logging.basicConfig(filename='server.log', level=logging.DEBUG)
 	now = datetime.datetime.now()	
 	logging.info('\n\n--Program Inititated at %s', str(now))			
-
 
 	# Check if anything is tbs, else exit
 	if ((args.sendFile is None) and (args.message is None)):
@@ -25,19 +26,29 @@ def main(args):
 		logging.info('Domain request chaff loaded from file')		
 	else:
 		chaff = ['www.google.com']
-		logging.info('No fomain request chaff provided, using www.google.com')
+		logging.info('No Domain request chaff provided, using www.google.com')
 
 	# If direct message is specified, send it
 	if (args.message is not None):
-		logging.info('Direct message \'%s\' tbs', args.message)
+		logging.info('Direct message \'%s\' to be sent', args.message)
 		for c in args.message:
-			sendByte(c, args.targetIP, chaff) # Remember to pass a list of one
+			sendByte(c, args.targetIP, chaff, args.randomDelay) # Remember to pass a list of one
 		logging.info('Direct message send complete')
 
 	# If file is specified, send it
+	if (args.sendFile is not None):
+		logging.info('%s to be sent', args.sendFile)
+		f = open(args.sendFile, 'rb')
+		try:
+			byte = f.read(1)
+			while byte != "":
+				sendByte(byte, args.targetIP, chaff, args.randomDelay)
+				byte = f.read(1)
+		finally:
+			f.close()
 
-def sendByte(c, targetIP, chaff):	
-	dest = '8.8.8.8' #Change to variable		
+def sendByte(c, targetIP, chaff, randomDelay):
+	dest = '8.8.8.8' #Assumes google DNS server @TODO
 	drequest = chaff[random.randint(0,len(chaff)-1)]	
 
 	#Checksum chaff for tID encoding
@@ -51,7 +62,10 @@ def sendByte(c, targetIP, chaff):
 		/DNS(id=tID, qd=DNSQR(qname=drequest))
 
 	send(spoofPacket, verbose=False)
-	logging.info('Sending packet with ID=%x to Target=%s with domain request=%s', tID, targetIP, drequest)
+	logging.info('Sending packet with ID=%x to Target=%s with domain request=%s', tID, targetIP, drequest)	
+
+	#Delay for random amount of time in range randomDelay
+	time.sleep(random.uniform(0,randomDelay))
 
 if __name__ == '__main__':
 	#Generate argparse menu
@@ -59,7 +73,7 @@ if __name__ == '__main__':
 
 	# Link chaff option
 	parser.add_argument('--rchaff', metavar='c',
-		dest='rchaff', default='domain-chaff.txt',
+		dest='rchaff', default=None,
 		help='File name of DNS request url chaff')
 
 	# File Send
@@ -74,7 +88,8 @@ if __name__ == '__main__':
 	parser.add_argument('--targetIP', metavar='t',
 		dest='targetIP', help='Target IP in format ')
 
-	parser.add_argument('--randomDelay', metavar='d')
+	parser.add_argument('--randomDelay', metavar='d', type=int,
+		dest='randomDelay', default = 0, help='Delay between DNS requests')
 
 	args = parser.parse_args()
 
